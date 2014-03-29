8a5f597a4105e4ba6646975c26362ecd765a9399
xuhao <rockflyme@gmail.com>
2014-03-29 09:19:52 +0800

    Add bayes.py

---
+++ bayes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+def loadDataSet():
+	postingList = [['my', 'dog', 'has', 'flea', \
+		        'problems', 'help', 'please'],
+		       ['maybe', 'not', 'take', 'him', \
+		        'to', 'dog', 'park', 'stupid'],
+		       ['my', 'dalmation', 'is', 'so', 'cute', \
+		        'I', 'love', 'it'],
+		       ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+		       ['mr', 'licks', 'ate', 'my', 'steak', 'how', \
+		        'to', 'stop', 'him'],
+		       ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+	classVec = [0, 1, 0, 1, 0, 1]
+	return postingList, classVec
